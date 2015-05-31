31f352af247b0d651e9314dc67075a8eef34c098
logicalguess <logicalguess@gmail.com>
2015-05-31 11:12:24 -0400

    main.py

---
+++ code/main.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+import matplotlib.pyplot as plt
+
+dfs = {}
+for name in ['train', 'test']:
+    df = pd.read_csv('../data/%s.csv' % name)
+    df['_data'] = name
+    dfs[name] = df
+
+# combine train and test data into one df
+df = dfs['train'].append(dfs['test'])
+
+# lowercase column names
+df.columns = map(str.lower, df.columns)
+
+# parse datetime colum & add new time related columns
+dt = pd.DatetimeIndex(df['datetime'])
+df.set_index(dt, inplace=True)
+
+# logarithmic transformation of dependent cols
+# (adding 1 first so that 0 values don't become -inf)
+for col in ['casual', 'registered', 'count']:
+    df['%s_log' % col] = np.log(df[col] + 1)
+
+df['date'] = dt.date
+df['day'] = dt.day
+df['month'] = dt.month
+df['year'] = dt.year
+df['hour'] = dt.hour
+df['dow'] = dt.dayofweek
+df['woy'] = dt.weekofyear
+
+# interpolate weather, atemp, humidity
+df["weather"] = df["weather"].interpolate(method='time').apply(np.round)
+df["temp"] = df["temp"].interpolate(method='time')
+df["atemp"] = df["atemp"].interpolate(method='time')
+df["humidity"] = df["humidity"].interpolate(method='time').apply(np.round)
+df["windspeed"] = df["windspeed"].interpolate(method='time')
+
+# add a count_season column using join
+by_season = df[df['_data'] == 'train'].groupby('season')[['count']].agg(sum)
+by_season.columns = ['count_season']
+df = df.join(by_season, on='season')
+
+print by_season
+
+def get_day(day_start):
+    day_end = day_start + pd.offsets.DateOffset(hours=23)
+    return pd.date_range(day_start, day_end, freq="H")
+
+# tax day
+df.loc[get_day(pd.datetime(2011, 4, 15)), "workingday"] = 1
+df.loc[get_day(pd.datetime(2012, 4, 16)), "workingday"] = 1
+# thanksgiving friday
+df.loc[get_day(pd.datetime(2011, 11, 25)), "workingday"] = 0
+df.loc[get_day(pd.datetime(2012, 11, 23)), "workingday"] = 0
+# tax day
+df.loc[get_day(pd.datetime(2011, 4, 15)), "holiday"] = 0
+df.loc[get_day(pd.datetime(2012, 4, 16)), "holiday"] = 0
+
+# thanksgiving friday
+df.loc[get_day(pd.datetime(2011, 11, 25)), "holiday"] = 1
+df.loc[get_day(pd.datetime(2012, 11, 23)), "holiday"] = 1
+
+#storms
+df.loc[get_day(pd.datetime(2012, 5, 21)), "holiday"] = 1
+#tornado
+df.loc[get_day(pd.datetime(2012, 6, 1)), "holiday"] = 1
+
+df['peak'] = df[['hour', 'workingday']].apply(lambda x: (0, 1)[(x['workingday'] == 1 and  ( x['hour'] == 8 or 17 <= x['hour'] <= 18 or 12 <= x['hour'] <= 12)) or (x['workingday'] == 0 and  10 <= x['hour'] <= 19)], axis = 1)
+
+#sandy
+df['holiday'] = df[['month', 'day', 'holiday', 'year']].apply(lambda x: (x['holiday'], 1)[x['year'] == 2012 and x['month'] == 10 and (x['day'] in [30])], axis = 1)
+
+#christmas day and others
+df['holiday'] = df[['month', 'day', 'holiday']].apply(lambda x: (x['holiday'], 1)[x['month'] == 12 and (x['day'] in [24, 26, 31])], axis = 1)
+df['workingday'] = df[['month', 'day', 'workingday']].apply(lambda x: (x['workingday'], 0)[x['month'] == 12 and x['day'] in [24, 31]], axis = 1)
+
+df['ideal'] = df[['temp', 'windspeed']].apply(lambda x: (0, 1)[x['temp'] > 27 and x['windspeed'] < 30], axis = 1)
+df['sticky'] = df[['humidity', 'workingday']].apply(lambda x: (0, 1)[x['workingday'] == 1 and x['humidity'] >= 60], axis = 1)
+
+def get_rmsle(y_pred, y_actual):
+    diff = np.log(y_pred + 1) - np.log(y_actual + 1)
+    mean_error = np.square(diff).mean()
+    return np.sqrt(mean_error)
+
+
+def get_data():
+    data = df[df['_data'] == 'train'].copy()
+    return data
+
+
+def custom_train_test_split(data, cutoff_day=15):
+    train = data[data['day'] <= cutoff_day]
+    test = data[data['day'] > cutoff_day]
+
+    return train, test
+
+
+def prep_data(data, input_cols):
+    X = data[input_cols].as_matrix()
+    y_r = data['registered_log'].as_matrix()
+    y_c = data['casual_log'].as_matrix()
+
+    return X, y_r, y_c
+
+
+def predict_on_validation_set(model, input_cols):
+    data = get_data()
+
+    train, test = custom_train_test_split(data)
+
+    X_train, y_train_r, y_train_c = prep_data(train, input_cols)
+    X_test, y_test_r, y_test_c = prep_data(test, input_cols)
+
+    model_r = model.fit(X_train, y_train_r)
+    y_pred_r = np.exp(model_r.predict(X_test)) - 1
+
+    model_c = model.fit(X_train, y_train_c)
+    y_pred_c = np.exp(model_c.predict(X_test)) - 1
+
+    y_pred_comb = np.round(y_pred_r + y_pred_c)
+    y_pred_comb[y_pred_comb < 0] = 0
+
+    y_test_comb = np.exp(y_test_r) + np.exp(y_test_c) - 2
+
+    score = get_rmsle(y_pred_comb, y_test_comb)
+    return (y_pred_comb, y_test_comb, score)
+
+df_test = df[df['_data'] == 'test'].copy()
+
+# predict on test set & transform output back from log scale
+def predict_on_test_set(model, x_cols):
+    # prepare training set
+    df_train = df[df['_data'] == 'train'].copy()
+    X_train = df_train[x_cols].as_matrix()
+    y_train_cas = df_train['casual_log'].as_matrix()
+    y_train_reg = df_train['registered_log'].as_matrix()
+
+    # prepare test set
+    X_test = df_test[x_cols].as_matrix()
+
+    casual_model = model.fit(X_train, y_train_cas)
+    y_pred_cas = casual_model.predict(X_test)
+    y_pred_cas = np.exp(y_pred_cas) - 1
+    registered_model = model.fit(X_train, y_train_reg)
+    y_pred_reg = registered_model.predict(X_test)
+    y_pred_reg = np.exp(y_pred_reg) - 1
+    # add casual & registered predictions together
+    return y_pred_cas + y_pred_reg
+
+params = {'n_estimators': 1000, 'max_depth': 15, 'random_state': 0, 'min_samples_split' : 5, 'n_jobs': -1}
+rf_model = RandomForestRegressor(**params)
+rf_cols = [
+    'weather', 'temp', 'atemp', 'windspeed',
+    'workingday', 'season', 'holiday', 'sticky',
+    'hour', 'dow', 'woy', 'peak'
+]
+
+rf_pred = predict_on_test_set(rf_model, rf_cols)
+(rf_p, rf_t, rf_score) = predict_on_validation_set(rf_model, rf_cols)
+print rf_score
+
+params = {'n_estimators': 150, 'max_depth': 5, 'random_state': 0, 'min_samples_leaf' : 10, 'learning_rate': 0.1, 'subsample': 0.7, 'loss': 'ls'}
+gbm_model = GradientBoostingRegressor(**params)
+gbm_cols = [
+    'weather', 'temp', 'atemp', 'humidity', 'windspeed',
+    'holiday', 'workingday', 'season',
+    'hour', 'dow', 'year', 'ideal', 'count_season',
+    ]
+
+gbm_pred = predict_on_test_set(gbm_model, gbm_cols)
+(gbm_p, gbm_t, gbm_score) = predict_on_validation_set(gbm_model, gbm_cols)
+print gbm_score
+
+# the blend gives a better score on the leaderboard, even though it does not on the validation set
+y_p = np.round(.2*rf_p + .8*gbm_p)
+print get_rmsle(y_p, rf_t)
+
+y_pred = np.round(.2*rf_pred + .8*gbm_pred)
+# output predictions for submission
+df_test['count'] = y_pred
+final_df = df_test[['datetime', 'count']].copy()
+final_df.to_csv('../output/submit.csv', index=False)
